916747ad2a0c0591c761644e45a211e72a53ebdc
Ayush <agrawal.ayush89@gmail.com>
2021-09-24 15:07:12 +1000

    learner and courses

---
+++ index.js
@@ -0,0 +1,30 @@
+const learnerAndCourses = function (dataset) {
+    //assuming valid JSON so no duplicate learners
+    
+    let singleLearnerCourseList = [], dups = [];
+
+    //traverse through the json obj
+    for (let learner in dataset){
+        //check if it has courses
+        if(dataset[learner].length > 0){
+            let courses = dataset[learner];
+            let newCourses = [];
+            for(let i in courses){
+                //if already added then its a repeat course
+                let idx = singleLearnerCourseList.indexOf(courses[i]);
+                if(idx >= 0){
+                    dups.push(courses[i]);
+                    singleLearnerCourseList.splice(idx, 1);
+                } else if (!dups.includes(courses[i]) && 
+                           !newCourses.includes(courses[i])) {
+                    newCourses.push(courses[i]);
+                }
+            }
+            if(newCourses.length > 0) singleLearnerCourseList.push(...newCourses);
+        }
+    }
+    return singleLearnerCourseList;
+};
+
+module.exports = learnerAndCourses;
+
---
+++ test/test.js
@@ -0,0 +1,96 @@
+var learnerAndCourses = require('../index');
+var assert = require('assert');
+
+describe('Go1', function() {
+    describe('learner and courses', function() {
+        it('return all courses when only 1 learner', function() {
+            let a = 
+                {
+                    "Learner-0001": [
+                    "Course-0001",
+                    "Course-0002",
+                    "Course-0003"
+                    ]
+                };
+            assert.deepEqual(learnerAndCourses(a), [
+                "Course-0001",
+                "Course-0002",
+                "Course-0003"
+            ]);
+        });
+        it('return all courses when non-repeat', function() {
+            let a = 
+                    {
+                    "Learner-0001": [
+                        "Course-0001"
+                    ],
+                    "Learner-0002": [
+                        "Course-0002"
+                    ],
+                    "Learner-0003": [
+                        "Course-0003"
+                    ],
+                    "Learner-0004": [
+                        "Course-0004"
+                    ]
+                    };
+            assert.deepEqual(learnerAndCourses(a), [
+                "Course-0001",
+                "Course-0002",
+                "Course-0003",
+                "Course-0004"
+            ]);
+        });
+        it('return 1 & 7 courses when 2 - 6 repeated', function() {
+            let a = 
+            {
+                "Learner-0001": [
+                  "Course-0001",
+                  "Course-0002",
+                  "Course-0003"
+                ],
+                "Learner-0002": [
+                  "Course-0002",
+                  "Course-0003",
+                  "Course-0004"
+                ],
+                "Learner-0003": [
+                  "Course-0004",
+                  "Course-0005",
+                  "Course-0006"
+                ],
+                "Learner-0004": [
+                  "Course-0005",
+                  "Course-0006",
+                  "Course-0007"
+                ]
+              };
+            assert.deepEqual(learnerAndCourses(a), [
+                "Course-0001",
+                "Course-0007"
+            ]);
+        });
+        it('return 1 & 4 courses when 2 - 3 repeated', function() {
+            let a = {
+                "Learner-0001": [
+                  "Course-0001",
+                  "Course-0002",
+                  "Course-0003",
+                  "Course-0001",
+                  "Course-0002",
+                  "Course-0003"
+                ],
+                "Learner-0002": [
+                  "Course-0002",
+                  "Course-0003",
+                  "Course-0004"
+                ]
+              };
+            
+            assert.deepEqual(learnerAndCourses(a), [
+                "Course-0001",
+                "Course-0004"
+            ]);
+        });
+  });
+});
