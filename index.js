415030e42f93aacfcd0d7537a1103e3bc4f694da
Ayush <agrawal.ayush89@gmail.com>
2021-09-24 16:04:49 +1000

    quiz res

--- index.js
+++ index.js
@@ -26,8 +26,38 @@ const learnerAndCourses = function (dataset) {
     return singleLearnerCourseList;
 };
 
+const quizResults = function (answers, responses) {
+    let index = 1;
+    var counter = answers.map(a => 0);
+    //go through each response
+    for (let i in responses){
+        let response = responses[i];
+        if (typeof(response) == 'object' && response.length == answers.length){
+            let count = response.map((a, idx) => a === answers[idx]);
+            console.log(count)
+            counter = counter.map((c,i) => {
+                if(count[i]) c++;
+                return c;
+            })
+            console.log(counter)
+        }
+    }
+    index = counter.indexOf(Math.max(...counter));
+
+    return "The easiest question is index " + index;
+}
+
+const watchedCourses = function() {
+    return {
+        "Course_001" : "Course_002",
+        "Course_002" : "Course_003"
+    }
+}
+
 module.exports = 
 {
-    learnerAndCourses: learnerAndCourses
+    learnerAndCourses: learnerAndCourses,
+    quizResults: quizResults,
+    watchedCourses: watchedCourses 
 };
 
